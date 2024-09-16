a0100f8ae8a5f8d1f6279fd90d4a3d61fa9f728f
codemmk <2107228359@qq.com>
2024-09-16 16:03:59 +0800

    完成主题布局

--- functions.php
+++ functions.php
@@ -2,3 +2,12 @@
 // 主题框架
 require_once get_theme_file_path() .'/inc/codestar-framework/codestar-framework.php';
 require get_theme_file_path(). '/inc/settings.php';
+
+// 菜单设置
+function register_custom_menu() {
+  register_nav_menu( 'top', '顶部菜单' );
+  register_nav_menu( 'main', '导航菜单' );
+  register_nav_menu( 'right', '右侧菜单' );
+}
+
+add_action( 'after_setup_theme', 'register_custom_menu' );
--- index.php
+++ index.php
@@ -1,14 +1,45 @@
 <?php get_header(); ?>
 
 <main>
-  <?php
-  if (have_posts()) :
-    while (have_posts()) : the_post();
-      the_title('<h1>', '</h1>');
-      the_content();
-    endwhile;
-  endif;
-  ?>
+  <div class="layout">
+    <div class="left">
+      <?php wp_nav_menu(array('theme_location' => 'main','menu_class'=>'nav-menu top-menu icon','container'=>'ul')); ?>
+    </div>
+    <div class="center">
+      <?php wp_nav_menu(array('theme_location' => 'top','menu_class'=>'nav-menu top-menu icon','container'=>'ul')); ?>
+    </div>
+    <div class="rigjt">
+      <?php wp_nav_menu(array('theme_location' => 'right','menu_class'=>'nav-menu top-menu icon','container'=>'ul')); ?>
+    </div>
+  </div>
 </main>
+
+<style>
+.layout {
+  display: flex;
+  width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+  overflow: hidden;
+  box-sizing: border-box;
+  background: skyblue;
+}
+
+.left {
+  width: 300px;
+  background: red;
+  margin-right: 20px;
+}
+
+.center {
+  flex: 1;
+  background: green;
+}
+
+.right {
+  width: 300px;
+  background: blue;
+}
+</style>
 
 <?php get_footer(); ?>
