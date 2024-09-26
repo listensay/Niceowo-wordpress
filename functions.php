49efa4d901c0b7a000c77bc8a3366519f8550e12
codemmk <2107228359@qq.com>
2024-09-26 20:10:30 +0800

    自定义设置暴露

--- functions.php
+++ functions.php
@@ -11,3 +11,86 @@ function register_custom_menu() {
 }
 
 add_action( 'after_setup_theme', 'register_custom_menu' );
+
+add_action( 'graphql_register_types', function() {
+  // 注册自定义字段，将 Codestar Framework 的设置暴露出来
+  register_graphql_field( 'RootQuery', 'codestarSettings', [
+    'type' => 'CodestarSettings', // 自定义类型
+    'description' => 'Codestar Framework settings',
+    'resolve' => function() {
+      // 获取 Codestar Framework 的设置
+      $theme_options = get_option('Niceowo'); // 修改 'csf_theme_options' 为实际使用的选项名称
+
+      return [
+        // 站点描述
+        'description' => $theme_options['site_description'],
+        // 站点LOGO
+        'logo' => $theme_options['site_logo'],
+        // 站点标语
+        'slogan' => $theme_options['site_slogan'],
+        // 建站日期
+        'date' => $theme_options['site_build_date'],
+        // 版权信息
+        'copy' => $theme_options['site_copyright'],
+        // 社交信息 头像
+        'avatar' => $theme_options['avatar'],
+        // 社交信息 网名昵称
+        'nickname' => $theme_options['nickname'],
+        // 社交信息 邮箱
+        'email' => $theme_options['email'],
+        // 社交信息 关于自己
+        'about' => $theme_options['about'],
+        // 社交信息 社交链接
+        'social_links' => $theme_options['social_links'],
+      ];
+    }
+  ]);
+
+  // 注册 CodestarSettings 对象类型
+  register_graphql_object_type( 'CodestarSettings', [
+    'fields' => [
+      'description' => [
+        'type' => 'String',
+        'description' => '站点描述',
+      ],
+      'logo' => [
+        'type' => 'String',
+        'description' => '站点LOGO',
+      ],
+      'slogan' => [
+        'type' => 'String',
+        'description' => '站点标语',
+      ],
+      'date' => [
+        'type' => 'String',
+        'description' => '建站日期',
+      ],
+      'copy' => [
+        'type' => 'String',
+        'description' => '版权信息 支持HTML',
+      ],
+      'avatar' => [
+        'type' => 'String',
+        'description' => '社交信息 头像',
+      ],
+      'nickname' => [
+        'type' => 'String',
+        'description' => '社交信息 网名昵称',
+      ],
+      'email' => [
+        'type' => 'String',
+        'description' => '社交信息 邮箱',
+      ],
+      'about' => [
+        'type' => 'String',
+        'description' => '社交信息 关于自己',
+      ],
+      'social_links' => [
+        'type' => [
+          'list_of' => 'SocialLink'
+        ],
+        'description' => '社交信息 社交链接',
+      ]
+    ]
+  ]);
+});
