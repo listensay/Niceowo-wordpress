8fa0535ccd1be37a14a4862d61da3d5b1a72ca2c
codemmk <2107228359@qq.com>
2024-10-02 11:45:37 +0800

    delete: 删除主题更新功能

--- functions.php
+++ functions.php
@@ -8,8 +8,3 @@ require get_theme_file_path(). '/inc/works.php';
 require get_theme_file_path(). '/inc/basic.php';
 // Codestar Framework settings
 require get_theme_file_path(). '/inc/cfs.php';
-
-function my_theme_github_updater() {
-  require_once get_template_directory() . '/updater.php';
-}
-add_action('after_setup_theme', 'my_theme_github_updater');
