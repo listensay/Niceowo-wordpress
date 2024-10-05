896521fcf0dbf78b28f493c3b196fa6c627542eb
codemmk <2107228359@qq.com>
2024-10-05 15:56:39 +0800

    新增个人资料设置

--- inc/cfs.php
+++ inc/cfs.php
@@ -2,87 +2,120 @@
   // GraphQL
   add_action( 'graphql_register_types', function() {
     // 注册自定义字段，将 Codestar Framework 的设置暴露出来
-    register_graphql_field( 'RootQuery', 'website', [
-      'type' => 'website', // 自定义类型
-      'description' => 'Codestar Framework settings',
-      'resolve' => function() {
-        $theme_options = get_option('Niceowo');
-        return $theme_options; 
-      }
-    ]);
+  register_graphql_field('RootQuery', 'website', [
+    'type' => 'website', 
+    'description' => 'Codestar Framework settings',
+    'resolve' => function() {
+      $theme_options = get_option('Niceowo');
+      return $theme_options;
+    }
+  ]);
 
-    // 注册 SocialLink 对象类型
-    register_graphql_object_type( 'SocialLink', [
-      'description' => '社交链接详情',
-      'fields' => [
-          'name' => [
-              'type' => 'String',
-              'description' => '社交网络的名称',
-          ],
-          'icon' => [
-              'type' => 'String',
-              'description' => '社交网络的图标',
-          ],
-          'url' => [
-              'type' => 'String',
-              'description' => '社交网络的链接地址',
-          ],
-      ]
-    ]);
 
-    // 注册 CodestarSettings 对象类型
-    register_graphql_object_type( 'website', [
-      'fields' => [
-        'description' => [
-          'type' => 'String',
-          'description' => '站点描述',
+  // 注册 SocialLink 对象类型
+  register_graphql_object_type( 'SocialLink', [
+    'description' => '社交链接详情',
+    'fields' => [
+        'name' => [
+            'type' => 'String',
+            'description' => '社交网络的名称',
         ],
-        'logo' => [
-          'type' => 'String',
-          'description' => '站点LOGO',
+        'icon' => [
+            'type' => 'String',
+            'description' => '社交网络的图标',
         ],
-        'slogan' => [
-          'type' => 'String',
-          'description' => '站点标语',
+        'url' => [
+            'type' => 'String',
+            'description' => '社交网络的链接地址',
         ],
-        'build_date' => [
-          'type' => 'String',
-          'description' => '建站日期',
+    ]
+  ]);
+
+  // 注册 Profile 对象类型
+  register_graphql_object_type( 'Profile', [
+    'description' => '个人资料',
+    'fields' => [
+        'key' => [
+            'type' => 'String',
+            'description' => '主题',
         ],
-        'copyright' => [
-          'type' => 'String',
-          'description' => '版权信息 支持HTML',
+        'value' => [
+            'type' => 'String',
+            'description' => '内容 支持HTML',
         ],
-        'avatar' => [
+        'color'=> [
           'type' => 'String',
-          'description' => '社交信息 头像',
-        ],
-        'nickname' => [
-          'type' => 'String',
-          'description' => '社交信息 网名昵称',
-        ],
-        'email' => [
-          'type' => 'String',
-          'description' => '社交信息 邮箱',
-        ],
-        'about' => [
-          'type' => 'String',
-          'description' => '社交信息 关于自己',
-        ],
-        'social_links' => [
-          'type' => ['list_of' => 'SocialLink'],
-          'description' => '社交网络链接列表',
-          'resolve' => function($root) {
-            if (empty($root['social_links'])) {
-              return [];
-            }
-            if (is_null($root['social_links'])) {
-              return [];
-            } else {
-              return $root['social_links'];
-            }
-          }
+          'description' => '颜色',
         ]
-      ]
-    ]);
-  });
+    ]
+  ]);
+
+  // 注册 CodestarSettings 对象类型
+  register_graphql_object_type( 'website', [
+    'fields' => [
+      'description' => [
+        'type' => 'String',
+        'description' => '站点描述',
+      ],
+      'logo' => [
+        'type' => 'String',
+        'description' => '站点LOGO',
+      ],
+      'slogan' => [
+        'type' => 'String',
+        'description' => '站点标语',
+      ],
+      'build_date' => [
+        'type' => 'String',
+        'description' => '建站日期',
+      ],
+      'copyright' => [
+        'type' => 'String',
+        'description' => '版权信息 支持HTML',
+      ],
+      'avatar' => [
+        'type' => 'String',
+        'description' => '社交信息 头像',
+      ],
+      'nickname' => [
+        'type' => 'String',
+        'description' => '社交信息 网名昵称',
+      ],
+      'email' => [
+        'type' => 'String',
+        'description' => '社交信息 邮箱',
+      ],
+      'about' => [
+        'type' => 'String',
+        'description' => '社交信息 关于自己',
+      ],
+      'social_links' => [
+        'type' => ['list_of' => 'SocialLink'],
+        'description' => '社交网络链接列表',
+        'resolve' => function($root) {
+          if (empty($root['social_links'])) {
+            return [];
+          }
+          if (is_null($root['social_links'])) {
+            return [];
+          } else {
+            return $root['social_links'];
+          }
+        }
+      ],
+      'profile' => [
+        'type' => ['list_of' => 'Profile'],
+        'description' => '个人资料列表',
+        'resolve' => function($root) {
+          if (empty($root['profile'])) {
+            return [];
+          }
+          if (is_null($root['profile'])) {
+            return [];
+          } else
+          return $root['profile'];
+        }
+      ],
+    ]
+  ]);
+});
